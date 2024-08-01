f7ee3527c5f5aaf8d2146b2a4f78cc455f26592e
tequ <git@tequ.dev>
2024-08-02 00:44:55 +0900

    remove python code

--- xrpld_netgen/deploykit/ansible/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# ./run.sh
-export ANSIBLE_HOST_KEY_CHECKING=False
-ansible -i hosts.txt all -u ubuntu -m ping
-ansible-playbook -i hosts.txt deps.yml
-ansible-playbook -i hosts.txt main.yml
--- xrpld_netgen/deploykit/nginx.dockerfile
+++
@@ -1,5 +0,0 @@
-FROM nginx:latest
-
-COPY vl.json /usr/share/nginx/html/vl.json
-
-EXPOSE 80
